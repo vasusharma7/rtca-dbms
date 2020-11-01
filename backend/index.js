e7afe8bf2e78e702a4f79d89323ac50c09f09516
Vasu Sharma <vasusharma2017@outlook.comwq>
2020-11-02 00:24:27 +0530

    added images

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,16 @@
 const express = require("express");
 const cors = require("cors");
-
+const path = require("path");
 const socket = require("socket.io");
+const convert = require("atob");
 const axios = require("axios");
+const fs = require("fs");
 const { saveMessage } = require("./main.js");
 const app = express();
 app.use(express.json({ limit: "50mb" }));
 app.use(cors());
 app.options("*", cors());
+app.use(express.static(path.join(__dirname, "public")));
 const http = require("http").Server(app);
 
 app.get("/", (req, res) => res.send("Hello World!"));
@@ -35,6 +38,20 @@ io.on("connection", function(socket) {
     io.emit("new user", [...activeUsers]);
   });
   socket.on("new message", (data) => {
+    io.emit("new message", data);
+    saveMessage(data);
+  });
+  socket.on("new image", (data) => {
+    io.emit("new image", data.image);
+    let image = data.image.base64.replace(/^data:image\/\w+;base64,/, "");
+    let fileName = new Date().getTime().toString() + data.image.name.toString();
+    fs.writeFileSync(
+      "./public/images/" + fileName,
+      image,
+      { encoding: "base64" },
+      function(err) {}
+    );
+    data["image"] = "images/" + fileName;
     io.emit("new message", data);
     saveMessage(data);
   });
--- backend/routes/chat.js
+++ backend/routes/chat.js
@@ -56,9 +56,9 @@ router.post("/userMessages", (req, res) => {
   let group_user_message = dm === true ? 0 : 1;
   let query = "";
   if (group_user_message) {
-    query = `SELECT contents.message_content,messages.date_time,messages.phone_number,messages.userID FROM messages inner join messageContent as contents on messages.messageID = contents.messageID where messages.groupID="${group}" AND messages.group_user_message=${group_user_message} order by messages.date_time;`;
+    query = `SELECT contents.file_as_blob,contents.message_content,messages.date_time,messages.phone_number,messages.userID FROM messages inner join messageContent as contents on messages.messageID = contents.messageID where messages.groupID="${group}" AND messages.group_user_message=${group_user_message} order by messages.date_time;`;
   } else {
-    query = `SELECT contents.message_content,messages.date_time,messages.phone_number,messages.userID FROM messages inner join messageContent as contents on messages.messageID = contents.messageID where ((messages.phone_number="${id}" and messages.userID = "${user}") or (messages.phone_number = "${user}" and messages.userID = "${id}")) and messages.groupID="${group}" AND messages.group_user_message=${group_user_message} order by messages.date_time;`;
+    query = `SELECT contents.file_as_blob,contents.message_content,messages.date_time,messages.phone_number,messages.userID FROM messages inner join messageContent as contents on messages.messageID = contents.messageID where ((messages.phone_number="${id}" and messages.userID = "${user}") or (messages.phone_number = "${user}" and messages.userID = "${id}")) and messages.groupID="${group}" AND messages.group_user_message=${group_user_message} order by messages.date_time;`;
   }
   console.log(query);
   conn.query(query, (err, result) => {
@@ -66,13 +66,13 @@ router.post("/userMessages", (req, res) => {
       console.log(err);
       throw err;
     }
-    // console.log(result);
     const data = [];
     result.forEach((record, key) => {
       data.push({
         id: key + 1,
         author: record.phone_number,
         message: record.message_content,
+        image: record.file_as_blob,
         timestamp: record.date_time,
       });
     });
@@ -80,7 +80,7 @@ router.post("/userMessages", (req, res) => {
   });
 });
 router.post("/saveMessage", (req, res) => {
-  let { from, to, message, id, dm, group } = req.body;
+  let { from, to, message, id, dm, group, image } = req.body;
   console.log("save", req.body);
   id = id.toString();
   id = parseInt(id.substring(id.length - 9));
@@ -89,6 +89,7 @@ router.post("/saveMessage", (req, res) => {
     .slice(0, 19)
     .replace("T", " ");
   let query = "";
+
   if (dm) {
     query = `INSERT INTO messages (messageID,phone_number,date_time,group_user_message,userID,groupID) VALUES ("${id}","${from}", "${time}", 0,"${to}","${group}");`;
   } else {
@@ -99,16 +100,20 @@ router.post("/saveMessage", (req, res) => {
       if (err) {
         console.log(err);
       }
-      conn.query(
-        `INSERT INTO messageContent (messageID,message_content) VALUES ("${id}","${message}");`,
-        (err, result) => {
-          if (err) {
-            console.log(err);
-          }
-          console.log("message saved successfully");
-          return res.status(200).json("Message Saved Successfully");
+      let query = "";
+      if (image) {
+        query = `INSERT INTO messageContent (messageID,message_content,file_as_blob) VALUES ("${id}","${message}","${image}");`;
+      } else {
+        query = `INSERT INTO messageContent (messageID,message_content) VALUES ("${id}","${message}");`;
+      }
+
+      conn.query(query, (err, result) => {
+        if (err) {
+          console.log(err);
         }
-      );
+        console.log("message saved successfully");
+        return res.status(200).json("Message Saved Successfully");
+      });
     });
   } catch (err) {
     console.log(err);
