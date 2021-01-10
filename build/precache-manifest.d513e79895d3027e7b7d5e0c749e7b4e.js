64419008fc84e047adeaaad29886a85bed9cc43b
Vasu Sharma <vasusharma2017@outlook.comwq>
2021-01-10 22:34:48 +0530

    build

---
+++ build/precache-manifest.d513e79895d3027e7b7d5e0c749e7b4e.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "f7389c370df149b65851",
+    "url": "/static/css/main.27b8e1b0.chunk.css"
+  },
+  {
+    "revision": "f7389c370df149b65851",
+    "url": "/static/js/main.f7389c37.chunk.js"
+  },
+  {
+    "revision": "529a6a9b3e1c05e09e92",
+    "url": "/static/css/1.e37e3629.chunk.css"
+  },
+  {
+    "revision": "529a6a9b3e1c05e09e92",
+    "url": "/static/js/1.529a6a9b.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "662ccd92f707452c20deeaccfee2b5c5",
+    "url": "/static/media/background.662ccd92.jpg"
+  },
+  {
+    "revision": "7ad935d5af941b6a68a541fac8f04dd5",
+    "url": "/static/media/ring.7ad935d5.mp3"
+  },
+  {
+    "revision": "02495e1cb43d33c68eb4034a271cfc48",
+    "url": "/static/media/logo.02495e1c.png"
+  },
+  {
+    "revision": "f11b103cc6906f8153b39bab78c2487e",
+    "url": "/static/media/Cross.f11b103c.svg"
+  },
+  {
+    "revision": "73adc9ffaf75383691c0ebbaf4019958",
+    "url": "/static/media/back.73adc9ff.png"
+  },
+  {
+    "revision": "eae63c0a705c6cebd7bdeccc9cd62457",
+    "url": "/static/media/background.eae63c0a.jpg"
+  },
+  {
+    "revision": "9b9332cf3e31274d857c7a9254167b95",
+    "url": "/index.html"
+  }
+];
